b2c88d3edfd351789607d013eb7e80a75c1cb627
AKir <kralt@yandex.ru>
2023-08-01 13:24:57 +0530

    Done Lena 1

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -17,32 +17,53 @@ public class Main {
         double average = 0;
         double smallest = 0;
         //todo дописать логику программы ниже.
-        if (first > second && first > third) {
+//        if (first > second && first > third) {
+//            greatest = first;
+//        } else if (second > first && second > third) {
+//            greatest = second;
+//        } else if (third > first && third > second) {
+//            greatest = third;
+//        }
+//        if (first < second && first < third) {
+//            smallest = first;
+//        } else if (second < first && second < third) {
+//            smallest = second;
+//        } else if (third < first && third < second) {
+//            smallest = third;
+//        }
+//        if ((first > second && first < third) || (first < second && first > third)) {
+//            average = first;
+//        } else if ((second > first && second < third) || (second < first && second > third)) {
+//            average = second;
+//        } else if ((third > first && third < second) || (third < first && third > second)) {
+//            average = third;
+//        }
+        if (first > second && first > third)
             greatest = first;
-        } else if (second > first && second > third) {
+        else if (second > first && second > third)
             greatest = second;
-        } else if (third > first && third > second) {
-            greatest = third;
-        }
-        if (first < second && first < third) {
+        else greatest = third;
+
+        if (first < second && first < third)
             smallest = first;
-        } else if (second < first && second < third) {
+        else  if (second < first && second < third)
             smallest = second;
-        } else if (third < first && third < second) {
-            smallest = third;
-        }
-        if ((first > second && first < third) || (first < second && first > third)) {
+        else smallest = third;
+
+        if (first < second && first > third || first > second && first < third)
             average = first;
-        } else if ((second > first && second < third) || (second < first && second > third)) {
+        else if (second < first && second > third || second > first && second < third)
             average = second;
-        } else if ((third > first && third < second) || (third < first && third > second)) {
-            average = third;
-        }
+        else average = third;{
+
+
+
 //
 //
         System.out.println("Наибольший вес: " + greatest);
         System.out.println("Средний вес: " + average);
         System.out.println("Наименьший вес: " + smallest);
+        }
     }
 
     }
